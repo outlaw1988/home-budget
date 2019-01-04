85d36e3fa9977514909e75e61d4e8cef47575c89
Jakub Zamojski <kuba.zamojski@gmail.com>
2019-01-04 20:14:10 +0100

    Initial view

---
+++ src/main/java/com/homebudget/homebudget/HomebudgetApplication.java
@@ -0,0 +1,14 @@
+package com.homebudget.homebudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebudgetApplication.class, args);
+	}
+
+}
+
